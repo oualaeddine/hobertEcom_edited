c6030e4f910720b5c2f0e06bc2b9d58f24789d63
Berrehal Ouala Eddine <berrehaloualaeddine@gmail.com>
2020-12-15 10:15:04 +0100

    db conf

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ECommerce/lahcen/hobertEcom_edited/';
+$config['base_url'] = 'https://info-tech.shop/';
 
 /*
 |--------------------------------------------------------------------------
