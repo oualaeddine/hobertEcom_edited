eabd20d96899c334e9626a72d53e55bee75d9328
Berrehal Ouala Eddine <berrehaloualaeddine@gmail.com>
2020-12-20 14:40:20 +0100

    footer hotfix

--- themes/hobrtEcom/footer.php
+++ themes/hobrtEcom/footer.php
@@ -7,9 +7,6 @@
                 <p>&copy;كل الحقوق محفوظة
                     <a href="https://info-tech.shop/" target="_blanck" style="color: #fff;">info-tech.shop</a>
                     <?php echo date("Y"); ?>.</p>
-                <a href="https://info-tech.shop/" target="_blanck" style="color: #fff;">info-tech.shop</a>
-                <a href="tel:0770878161" target="_blanck">0770878161 </a>
-
             </div>
             <div class="lx-clear-fix"></div>
         </div>
