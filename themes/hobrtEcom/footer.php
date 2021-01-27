6b15a1f5d34c98b4814c1e51684ded33437dc409
Berrehal Ouala Eddine <berrehaloualaeddine@gmail.com>
2021-01-28 00:10:17 +0100

    footer

--- themes/hobrtEcom/footer.php
+++ themes/hobrtEcom/footer.php
@@ -8,7 +8,7 @@
         <div class="lx-footer-bottom-content">
             <div class="lx-copyright">
                 <p>&copy;كل الحقوق محفوظة
-                    <a href="https://info-tech.shop/" target="_blanck" style="color: #fff;">info-tech.shop</a>
+                    <a href="https://berrehal.com/" target="_blanck" style="color: #fff;">berrehal.com</a>
                     <?php echo date("Y"); ?>.</p>
             </div>
             <div class="lx-clear-fix"></div>
